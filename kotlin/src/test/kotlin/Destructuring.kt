fd475189b41e669f7fda2657c6ddf661e0286b4d
Manuel Riezebosch <mriezebosch@gmail.com>
2022-10-08 12:01:54 +0200

    feat(kotlin): destructuring types

---
+++ kotlin/src/test/kotlin/Destructuring.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+// https://kotlinlang.org/docs/destructuring-declarations.html
+class Destructuring {
+    private fun create() = Pair(1, "b")
+
+    @Test
+    fun pair() {
+        val (a, b) = create()
+
+        assertEquals(1, a)
+        assertEquals("b", b)
+    }
+
+    @Test
+    fun triple() {
+        val (a, b) = Triple(1, 2, 3)
+
+        assertEquals(1, a)
+        assertEquals(2, b)
+    }
+
+    data class Data(val x: Int, val y: String, val z: Boolean)
+
+    @Test
+    fun destructure() {
+        val (a, _, c) = Data(1, "b", true)
+
+        assertEquals(1, a)
+        assertTrue(c)
+    }
+}
