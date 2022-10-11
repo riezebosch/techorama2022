7e524a2b8aa009374dc83783ca37b72ec0e33979
Manuel Riezebosch <mriezebosch@gmail.com>
2022-10-11 14:40:18 +0000

    feat(kotlin): infix

--- kotlin/src/test/kotlin/Extensions.kt
+++ kotlin/src/test/kotlin/Extensions.kt
@@ -1,18 +1,26 @@
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Extensions {
+    private fun Int.translate() = when (this) {
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        else -> "unknown number"
+    }
+
     @Test
     fun test() {
-        assertEquals("one", 1.something())
-        assertEquals("two", 2.something())
-        assertEquals("three", 3.something())
+        assertEquals("one", 1.translate())
+        assertEquals("two", 2.translate())
+        assertEquals("three", 3.translate())
     }
-}
 
-private fun Int.something() = when(this) {
-    1 -> "one"
-    2 -> "two"
-    3 -> "three"
-    else -> "unknown number"
+    private infix fun Int.subtract(item: Int) = this - item
+
+    @Test
+    fun infix() {
+        4 subtract 1 shouldBe 3
+    }
 }
