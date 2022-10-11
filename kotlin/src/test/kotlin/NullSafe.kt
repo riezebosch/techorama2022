14cfd03095ca33c6ab87230754e03733a935b401
Manuel Riezebosch <mriezebosch@gmail.com>
2022-10-11 15:57:42 +0200

    feat(kotlin): null-safe

---
+++ kotlin/src/test/kotlin/NullSafe.kt
@@ -0,0 +1,30 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class NullSafe {
+    @Test
+    fun safe() {
+        val input: Int? = null
+        val exception = assertThrows<NullPointerException> {
+            input!!
+        }
+
+        exception.stackTrace[0].lineNumber shouldBe 10
+    }
+
+    fun calc(input: Int) = 2 * input
+
+    @Test
+    fun calling() {
+        val input: Int? = null
+        input shouldBe null
+
+//        calc(input!!)
+    }
+
+    fun calc(input: Int?) = 2 * (input ?: 1)
+
+    @Test
+    fun elvis() = calc(null) shouldBe 2
+}
